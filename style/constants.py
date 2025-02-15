4d6a26d5a1c21b1cb27f92325dde6deef0340eae
Pettro98 <ya.skywalker@yandex.ru>
2025-02-16 01:55:44 +0300

    Draft of style options

---
+++ style/constants.py
@@ -0,0 +1,22 @@
+# Sequence values follow CSS rules:
+# - margins/paddings: (top, right, bottom, left)
+# - corner-radius: (top-left, top-right, bottom-right, bottom-left)
+# - box-size: (width, height)
+# - color: (red, green, blue, [alpha = 255])
+
+# TODO: fill in real values
+MESSAGE_FONT_SIZE = 25
+# MESSAGE_TEXT_LINE_HEIGHT = MESSAGE_FONT_SIZE
+MESSAGE_TEXT_COLOR = (255, 255, 255)
+MESSAGE_BUBBLE_BACKGROUND_COLOR = (0, 0, 0)
+MESSAGE_BUBBLE_PADDING = (10, 10, 10, 10)
+MESSAGE_BUBBLE_GAP = 3 # gap between subsequent message bubbles
+
+USERNAME_FONT_SIZE = 25
+# USERNAME_LINE_HEIGHT = USERNAME_FONT_SIZE
+USERNAME_TEXT_COLOR = (255, 128, 128)
+USERNAME_BOTTOM_MARGIN = 0 # gap between username and text of top message
+
+AVATAR_SIZE = (50, 50)
+GENERATED_AVATAR_FONT_SIZE = 30
+AVATAR_TEXT_COLOR = (255, 255, 255)
